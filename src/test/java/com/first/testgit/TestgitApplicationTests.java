b003e544ed484af04b3498748932ecac44a7e183
bdjiaxingxu <1415016185@qq.com>
2020-12-28 17:26:49 +0800

    测试

---
+++ src/test/java/com/first/testgit/TestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
